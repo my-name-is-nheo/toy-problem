37947e026e95f3310b9a014890140560c5b85001
Nicholas Heo <nicholas.heo95@gmail.com>
2020-05-23 14:05:51 -0400

    finished whiteboarding

--- doubleExist.js
+++ doubleExist.js
@@ -1,15 +1,24 @@
 // Given an array arr of integers, check if there exists two integers N and M such that N is the double of M ( i.e. N = 2 * M).
 var checkIfExist = function (arr) {
   /*
-I
-O
-C
+I - array
+O -boolean
+C -
+i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
 E
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: N = 10 is the double of M = 5,that is, 10 = 2 * 5.
 
+Input: arr = [7,1,14,11]
+Output: true
+Explanation: N = 14 is the double of M = 7,that is, 14 = 2 * 7.
 
-
-
-
+Input: arr = [3,1,7,11]
+Output: false
+Explanation: In this case does not exist N and M, such that N = 2 * M.
 
 
    */
