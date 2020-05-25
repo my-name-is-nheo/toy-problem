ef0eea81798d219e8c89247b308298a15f9c5918
Nicholas Heo <nicholas.heo95@gmail.com>
2020-05-25 14:36:07 -0400

    whiteboard

--- validParentheses.js
+++ validParentheses.js
@@ -9,4 +9,26 @@ Note that an empty string is also considered valid.
 
 Example 1:
 */
-var isValid = function (s) {};
+var isValid = function (s) {
+  /*
+  I - string
+  O - boolean
+  C - empty string is considered valid
+  E
+  Input: "()"
+  Output: true
+  Input: "()[]{}"
+  Output: true
+  Input: "(]"
+  Output: false
+  Input: "([)]"
+  Output: false
+  Input: "{[]}"
+  Output: true
+
+
+
+
+
+   */
+};
