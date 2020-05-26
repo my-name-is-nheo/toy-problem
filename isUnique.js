8779d3c3bd4ce8d57bbf2aa6f681297ecad2d8a4
Nicholas Heo <nicholas.heo95@gmail.com>
2020-05-26 16:07:42 -0400

    attempt

--- isUnique.js
+++ isUnique.js
@@ -1,5 +1,12 @@
 // Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures
 
 var isUnique = function (s) {
-  var newString = s.split("");
+  var existObj = {};
+  for (var index = 0; index < s.length; index++) {
+    if (existObj[s[index]]) {
+      return false;
+    }
+    existObj[s[index]] = true;
+  }
+  return true;
 };
