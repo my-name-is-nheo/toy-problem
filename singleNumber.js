8da0c5e26d4435e9199de7c3a073dad97fa09f96
Your Name <you@example.com>
2020-05-29 21:38:48 -0400

    whiteboarding

--- singleNumber.js
+++ singleNumber.js
@@ -1,2 +1,12 @@
 // Given a non-empty array of integers, every element appears twice except for one. Find that single one.
-var singleNumber = function (array) {};
+var singleNumber = function (array) {
+  /*
+I - non-empty array
+O - integer
+C - none
+E ;
+[2,2,1] =1
+[2,2,3,1,3,3,0] =0
+
+*/
+};
