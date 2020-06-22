5d65cef2fd1e621f9ce37e781825e21a1c512548
Nicholas Heo <nicholas.heo95@gmail.com>
2020-06-22 14:52:08 -0400

    new commit

--- risingTemperature.js
+++ risingTemperature.js
@@ -11,5 +11,5 @@ Given a Weather table, write a SQL query to find all dates' Ids with higher temp
 +---------+------------------+------------------+
 
 */
-SELECT a.Id FROM Weather AS a, Weather AS b
-WHERE DATEDIFF(a.Date, b.Date)=1 AND a.Temperature > b.Temperature
+// SELECT a.Id FROM Weather AS a, Weather AS b
+// WHERE DATEDIFF(a.Date, b.Date)=1 AND a.Temperature > b.Temperature
