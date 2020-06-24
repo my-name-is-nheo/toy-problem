8323bff43602f527579618d7cd759bd8886d2fdf
Nicholas Heo <nicholas.heo95@gmail.com>
2020-06-24 17:01:54 -0400

    first commit

---
+++ smallerThanNumber.js
@@ -0,0 +1,18 @@
+/*
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+*/
+var smallerNumbersThanCurrent = function (nums) {
+  /*
+Input - array
+output - array
+constraints - 2<=nums.length <=500
+              0<nums[index]<=100
+edgecases
+Input: nums = [6,5,4,8]
+Output: [2,1,0,3]
+Input: nums = [7,7,7,7]
+Output: [0,0,0,0]
+*/
+};
