4304cb1e42f79c342974c1505ad295b583a4fac4
Nicholas Heo <nicholas.heo95@gmail.com>
2020-06-24 17:20:49 -0400

    attempt 1

--- smallerThanNumber.js
+++ smallerThanNumber.js
@@ -15,4 +15,24 @@ Output: [2,1,0,3]
 Input: nums = [7,7,7,7]
 Output: [0,0,0,0]
 */
+
+  if (nums.length > 0) {
+    var newArray = [];
+    for (var index = 0; index < nums.length; index++) {
+      var total = 0;
+      var currentElement = nums[index];
+      console.log("since is currentElement ", currentElement);
+      for (var p = 0; p < nums.length; p++) {
+        if (currentElement > nums[p] && p != index) {
+          total++;
+        }
+      }
+      console.log("this is total ", total);
+      newArray.push(total);
+    }
+  }
+  console.log(newArray);
+  return newArray;
 };
+
+smallerNumbersThanCurrent([7, 7, 7, 7]);
