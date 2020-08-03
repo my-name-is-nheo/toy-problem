7a6a17eba631d89485875d312d1ce228e80ca51c
Nicholas Heo <nicholas.heo95@gmail.com>
2020-08-02 20:48:15 -0400

    new

---
+++ splitAString.js
@@ -0,0 +1,12 @@
+// Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+// Given a balanced string s split it in the maximum amount of balanced strings.
+
+// Return the maximum amount of splitted balanced strings.
+
+var balancedStringSplit = function (s) {
+  //I - string
+  //O - integer
+  //C - i < s.length < 1000 , s[i] = L or R in string makes sense
+  //E
+};
