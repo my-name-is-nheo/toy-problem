d97c948d85973493da259a42be7f61e7316b1246
Nicholas Heo <nicholas.heo95@gmail.com>
2020-08-02 20:56:47 -0400

    solved

--- splitAString.js
+++ splitAString.js
@@ -9,4 +9,35 @@ var balancedStringSplit = function (s) {
   //O - integer
   //C - i < s.length < 1000 , s[i] = L or R in string makes sense
   //E
+  /*
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+
+Input: s = "RLLLLRRRLR"
+Output: 3
+Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+*/
+  let matches = 0;
+  let balance = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "R") {
+      balance -= 1;
+    } else if (s[i] === "L") {
+      balance += 1;
+    }
+
+    if (balance === 0) {
+      matches += 1;
+    }
+  }
+  console.log(`there are ${matches} matches`);
+  return `there are ${matches} matches`;
 };
+
+balancedStringSplit("RL");
+balancedStringSplit("RRLL");
+balancedStringSplit("RLRLLR");
+balancedStringSplit("RLRRLL");
+balancedStringSplit("L");
