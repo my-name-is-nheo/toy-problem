74cf104b5a8b475ae999a013c25204015c602d1c
Nicholas Heo <nicholas.heo95@gmail.com>
2020-08-03 19:25:51 -0400

    solved

--- primeFactors.js
+++ primeFactors.js
@@ -1,2 +1,17 @@
-function primeFactors(n) {}
+function primeFactors(n) {
+  var factors = [],
+    divisor = 2;
+
+  while (n > 2) {
+    if (n % divisor === 0) {
+      factors.push(divisor);
+      n /= divisor;
+      console.log(n);
+    } else {
+      divisor++;
+    }
+  }
+  console.log(factors);
+  return factors;
+}
 primeFactors(69); // equal [3,23]
