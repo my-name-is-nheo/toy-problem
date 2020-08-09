2ba947837ec9abb339b010b59943afeddffbabb3
Nicholas Heo <nicholas.heo95@gmail.com>
2020-08-09 16:49:21 -0400

    wooooow

--- greatCommonDivisor.js
+++ greatCommonDivisor.js
@@ -1,2 +1,12 @@
-function greatestCommonDivisor(x, y) {}
+function greatestCommonDivisor(x, y) {
+  if (y == 0) {
+    return x;
+  } else {
+    console.log("this the new y ", y);
+    console.log("this the new x & y ", x % y);
+
+    return greatestCommonDivisor(y, x % y);
+  }
+}
 //How would you find the greatest common divisor of two numbers?
+greatestCommonDivisor(14, 21);
