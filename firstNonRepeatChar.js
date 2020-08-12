8b81a36aabb803487f5bcd5eb24d9596c8aba3fe
Nicholas Heo <nicholas.heo95@gmail.com>
2020-08-12 00:03:58 -0400

    solved

--- firstNonRepeatChar.js
+++ firstNonRepeatChar.js
@@ -1,1 +1,17 @@
-function firstNonRepeatChar(string) {}
+function firstNonRepeatChar(string) {
+  var charCount = {};
+  for (var index = 0; index < string.length; index++) {
+    var currentLetter = string[index];
+    if (!charCount[currentLetter]) {
+      charCount[currentLetter] = 1;
+    } else {
+      charCount[currentLetter]++;
+    }
+  }
+
+  for (var j in charCount) {
+    if (charCount[j] === 1) {
+      return j;
+    }
+  }
+}
