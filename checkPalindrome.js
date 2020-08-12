eb8933ed27056629f327995fd214daae5e25b15b
Nicholas Heo <nicholas.heo95@gmail.com>
2020-08-12 11:42:42 -0400

    should work

--- checkPalindrome.js
+++ checkPalindrome.js
@@ -1,1 +1,5 @@
-function checkPalindrome(string) {}
+function checkPalindrome(string) {
+  // the question to ask. is the string equal  to the string.reverse?
+
+  return string == string.split("").reverse().join("");
+}
