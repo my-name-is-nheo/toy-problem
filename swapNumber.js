89acb6a5e084f922151cab1216d58c67dcf3e856
Nicholas Heo <nicholas.heo95@gmail.com>
2020-08-11 23:31:42 -0400

    solution.. pretty cool

--- swapNumber.js
+++ swapNumber.js
@@ -1,2 +1,10 @@
 // How would you swap two numbers without using a temporary variable?
-function swapNumber(a, b) {}
+function swapNumber(a, b) {
+  console.log(`this is before swap: a = ${a} b=${b}`);
+  b = b - a;
+  a = a + b;
+  b = a - b;
+  console.log(`this is after swap: a = ${a} b=${b}`);
+}
+
+swapNumber(2, 3);
