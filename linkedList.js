3a5f9a46c03f9252877d821d404d8d029a0ab50c
Nicholas Heo <nicholas.heo95@gmail.com>
2020-08-13 01:39:43 -0400

    linked list practice

---
+++ linkedList.js
@@ -0,0 +1,7 @@
+var LinkedList = function () {};
+
+//write methods as well
+LinkedList.prototype.addToTail = function (val) {};
+LinkedList.prototype.removeHead = function () {};
+LinkedList.prototype.contains = function (val) {};
+LinkedList.prototype.makeNode = function (val) {};
