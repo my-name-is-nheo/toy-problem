4496815485495e3b52013b33dfd97e3c6efa0b84
Nicholas Heo <nicholas.heo95@gmail.com>
2020-08-13 01:37:19 -0400

    solved

--- sumOfTwo.js
+++ sumOfTwo.js
@@ -1,1 +1,17 @@
-function sumFinder(arr, sum) {}
+// From an unsorted array, check weather there are any two numbers that will sum up to a given number.
+function sumFinder(arr, sum) {
+  var differ = {},
+    substract;
+
+  for (var i = 0; i < arr.length; i++) {
+    substract = sum - arr[i];
+
+    if (differ[substract]) return true;
+    else differ[arr[i]] = true;
+  }
+
+  return false;
+}
+
+console.log(sumFinder([6, 4, 3, 2, 1, 7], 9));
+console.log(sumFinder([6, 4, 3, 2, 1, 7], 2));
