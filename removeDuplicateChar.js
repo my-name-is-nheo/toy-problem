ccecb043bc58c159e18286a8b2d3a6dec6841949
Nicholas Heo <nicholas.heo95@gmail.com>
2020-08-17 22:10:52 -0400

    attempt

--- removeDuplicateChar.js
+++ removeDuplicateChar.js
@@ -1,1 +1,21 @@
-function removeDuplicateChar(str) {}
+function removeDuplicateChar(str) {
+  var finalObject = {};
+  var finalArray = [];
+  for (var index = 0; index < str.length; index++) {
+    var currentLetter = str[index];
+    if (!finalObject[currentLetter]) {
+      finalObject[currentLetter] = 1;
+    } else {
+      finalObject[currentLetter]++;
+    }
+  }
+  for (var key in finalObject) {
+    if (finalObject[key] === 1) {
+      finalArray.push(key);
+    }
+  }
+
+  return finalArray.join("");
+}
+
+console.log(removeDuplicateChar("Learn more javascript dude"));
