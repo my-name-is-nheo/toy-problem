39444bd94d875728a538a81e449f1e607584721d
Nicholas Heo <nicholas.heo95@gmail.com>
2020-09-07 20:23:58 -0400

    attempt O(n*m) time complexity

--- wordPattern.js
+++ wordPattern.js
@@ -23,4 +23,34 @@ Output: false
 Notes:
 You may assume pattern contains only lowercase letters, and str contains lowercase letters that may be separated by a single space.
 */
-var wordPattern = function (pattern, str) {};
+var wordPattern = function (pattern, str) {
+  // 2 inputs string, and string
+  // Output: boolean
+  // Constraints: both strings will be in lowercase
+  // E look above
+  var patternStorage = {};
+  var splittedPattern = pattern.split(""); //["a", "b", "b", "a"]
+  var splittedStr = str.split(" "); // ["dog", "dog" ,"dog","dog"]
+
+  for (var i = 0; i < splittedPattern.length; i++) {
+    var currentPattern = splittedPattern[i];
+    if (
+      patternStorage[currentPattern] === undefined &&
+      Object.values(patternStorage).indexOf(splittedStr[i]) === -1
+    ) {
+      patternStorage[currentPattern] = splittedStr[i];
+    } else {
+      if (patternStorage[currentPattern] !== splittedStr[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+console.log(wordPattern("abba", "dog cat cat dog")); //TRUE
+console.log(wordPattern("abba", "dog cat cat fish")); //FALSE
+console.log(wordPattern("aaaa", "dog cat cat dog")); //FALSE
+console.log(wordPattern("abba", "dog dog dog dog")); //FALSE <=== breaking..
+console.log(wordPattern("aaaa", "dog dog dog dog")); //TRUE
+console.log(wordPattern("aaaa", "dog cat cat cat"));
