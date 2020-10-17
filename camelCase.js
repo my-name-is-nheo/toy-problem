0c2ad4c5c1f6e965d3a885f9fa355c6b8a5d9c33
Nicholas Heo <nicholas.heo95@gmail.com>
2020-10-16 20:28:47 -0400

    attempt

--- camelCase.js
+++ camelCase.js
@@ -4,4 +4,15 @@ Complete the solution so that the function will break up camel casing, using a s
 Example
 solution("camelCasing")  ==  "camel Casing"
 */
-function solution(string) {}
+function solution(string) {
+  var newString = string.split("");
+  for (var index = 0; index < newString.length; index++) {
+    var currentChar = newString[index];
+    if (currentChar === currentChar.toUpperCase()) {
+      newString[index] = ` ${currentChar}`;
+    }
+  }
+  return newString.join("");
+}
+console.log(solution("camelCasing"));
+console.log(solution("hiNickAndKaren"));
