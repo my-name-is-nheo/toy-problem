aa1264da6fd35cac8c4d6da07c8e8c89da40e351
Nicholas Heo <nicholas.heo95@gmail.com>
2020-10-17 16:35:43 -0400

    attempt

--- alternateCap.js
+++ alternateCap.js
@@ -8,4 +8,27 @@ The input will be a lowercase string with no spaces.
 
 
 */
-function capitalize(s) {}
+function capitalize(s) {
+  var oddWord = "";
+  var evenWord = "";
+  while (evenWord.length < s.length) {
+    for (var index = 0; index < s.length; index++) {
+      if (index % 2 === 0) {
+        evenWord = evenWord + s[index].toUpperCase();
+      } else {
+        evenWord = evenWord + s[index];
+      }
+    }
+  }
+  while (oddWord.length < s.length) {
+    for (var index = 0; index < s.length; index++) {
+      if (index % 2 === 1) {
+        oddWord = oddWord + s[index].toUpperCase();
+      } else {
+        oddWord = oddWord + s[index];
+      }
+    }
+  }
+  return [evenWord, oddWord];
+}
+console.log(capitalize("abcdef"));
