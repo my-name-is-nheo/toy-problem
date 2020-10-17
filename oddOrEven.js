558fbcbf849c003b5abd67901744bf066858f8a1
Nicholas Heo <nicholas.heo95@gmail.com>
2020-10-17 16:01:13 -0400

    attempt

--- oddOrEven.js
+++ oddOrEven.js
@@ -11,4 +11,19 @@ odd_or_even([0, 1, 4])    ==  "odd"
 odd_or_even([0, -1, -5])  ==  "even"
 */
 
-var odd_or_even = () => {};
+var odd_or_even = (arr) => {
+  if (arr.length === 0) {
+    return "even";
+  }
+  var sum = 0;
+  for (var index = 0; index < arr.length; index++) {
+    sum += arr[index];
+  }
+  if (sum % 2 === 0) {
+    return "even";
+  }
+  return "odd";
+};
+console.log(odd_or_even([0]));
+console.log(odd_or_even([0, 1, 4]));
+console.log(odd_or_even([0, -1, -5]));
