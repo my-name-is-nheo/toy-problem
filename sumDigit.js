00087f02f0659678b44e21a6cb0174a798d100c8
Nicholas Heo <nicholas.heo95@gmail.com>
2020-10-17 21:52:02 -0400

    initial

---
+++ sumDigit.js
@@ -0,0 +1,8 @@
+/*
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits. For example:
+
+  sumDigits(10);  // Returns 1
+  sumDigits(99);  // Returns 18
+  sumDigits(-32); // Returns 5
+*/
+var sumDigits = (number) => {};
