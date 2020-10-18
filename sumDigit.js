dca4cf1294ede91f1f8edf7aec3719637d8c30ad
Nicholas Heo <nicholas.heo95@gmail.com>
2020-10-17 22:02:51 -0400

    attempt

--- sumDigit.js
+++ sumDigit.js
@@ -5,4 +5,17 @@ Write a function named sumDigits which takes a number as input and returns the s
   sumDigits(99);  // Returns 18
   sumDigits(-32); // Returns 5
 */
-var sumDigits = (number) => {};
+var sumDigits = (number) => {
+  var numberToString = Math.abs(number).toString();
+
+  var sum = 0;
+  for (var index = 0; index < numberToString.length; index++) {
+    sum += parseInt(numberToString[index]);
+  }
+
+  return sum;
+};
+
+console.log(sumDigits(10));
+console.log(sumDigits(99));
+console.log(sumDigits(-32));
