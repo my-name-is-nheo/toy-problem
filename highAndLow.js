cc1f5b3f9b99fe6f7dbe6214ac24279382ebe2f8
Nicholas Heo <nicholas.heo95@gmail.com>
2020-10-19 15:34:18 -0400

    attempt

--- highAndLow.js
+++ highAndLow.js
@@ -13,4 +13,15 @@ All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first.
 */
-function highAndLow(numbers) {}
+function highAndLow(numbers) {
+  var sortedSplittedArray = numbers.split(" ").sort((a, b) => {
+    return b - a;
+  });
+  return `${sortedSplittedArray[0]} ${
+    sortedSplittedArray[sortedSplittedArray.length - 1]
+  }`;
+}
+console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
+console.log(highAndLow("1 2 3 4 5"));
+console.log(highAndLow("1 2 -3 4 5"));
+console.log(highAndLow("1 9 3 4 -5"));
