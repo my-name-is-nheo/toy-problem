c4c68457f2e781f665a392a032ea3723ef84c951
Nicholas Heo <nicholas.heo95@gmail.com>
2020-10-21 18:24:47 -0400

    attempt

--- primeFactorsII.js
+++ primeFactorsII.js
@@ -9,5 +9,24 @@ with the p(i) in increasing order and n(i) empty if n(i) is 1.
 Example: n = 86240 should return "(2**5)(5)(7**2)(11)"
 */
 
-var primeFactorsII = (number) => {};
+var primeFactorsII = (number) => {
+  var divisible = 2;
+  var arrayOfDivisibles = [];
+  while (divisible <= number) {
+    if (number % divisible === 0) {
+      if (divisible === Math.sqrt(divisible) ** 2) {
+        arrayOfDivisibles.push(Math.sqrt(divisible));
+      } else {
+        arrayOfDivisibles.push(divisible);
+      }
+    }
+
+    divisible++;
+  }
+  console.log(
+    arrayOfDivisibles.filter((num) => {
+      return num % 1 === 0;
+    })
+  );
+};
 console.log(primeFactorsII(86240));
