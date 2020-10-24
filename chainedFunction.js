389b171d6162c263e422e6d0ccf8a7d03a1dc5a6
Nicholas Heo <nicholas.heo95@gmail.com>
2020-10-24 18:31:40 -0400

    attempt

--- chainedFunction.js
+++ chainedFunction.js
@@ -7,4 +7,37 @@ Should yield the same result as
 d(c(b(a(input))))
 */
 
-var chained = (functions) => {};
+var chained = (functions) => {
+  return function (input) {
+    return functions.reduce(function (input, fn) {
+      // reducer function accumulator, currentValue, currentIndex, and source Array
+      console.log(input, fn);
+      return fn(input);
+    }, input); //initial value
+  };
+};
+// console.log(chained([f1, f2, f3])(0));
+function f1(x) {
+  return x * 2;
+}
+function f2(x) {
+  return x + 2;
+}
+function f3(x) {
+  return Math.pow(x, 2);
+}
+
+function f4(x) {
+  return x.split("").concat().reverse().join("").split(" ");
+}
+function f5(xs) {
+  return xs.concat().reverse();
+}
+function f6(xs) {
+  return xs.join("_");
+}
+
+console.log(chained([f1, f2, f3])(0));
+console.log(chained([f1, f2, f3])(2));
+console.log(chained([f3, f2, f1])(2));
+console.log(chained([f4, f5, f6])("lorem ipsum"));
