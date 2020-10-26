c4ff30d1e57a9b8c06561a94962d3ed79dcc8509
Nicholas Heo <nicholas.heo95@gmail.com>
2020-10-26 19:04:16 -0400

    initial

--- kebabize.js
+++ kebabize.js
@@ -8,20 +8,22 @@ Notes:
 the returned string should only contain lowercase letters
 */
 
-var kebabize = (string) => {
-  var wordContainer = [];
-  var wordToPush = "";
-  for (var index = 0; index < string.length; index++) {
-    var currentChar = string[index];
-    if (currentChar === currentChar.toUpperCase()) {
-      wordContainer.push(wordToPush);
-      wordToPush = "";
-      wordToPush += currentChar;
-    } else {
-      wordToPush += currentChar;
-    }
-  }
-  console.log(wordContainer);
+var kebabize = (str) => {
+  // var wordContainer = [];
+  // var wordToPush = "";
+  // for (var index = 0; index < string.length; index++) {
+  //   var currentChar = string[index];
+  //   if (currentChar === currentChar.toUpperCase()) {
+  //     wordContainer.push(wordToPush);
+  //     wordToPush = "";
+  //     wordToPush += currentChar;
+  //   } else {
+  //     wordToPush += currentChar;
+  //   }
+  // }
+  // console.log(wordContainer);
+  var nonumber = str.replace(/\d/g, "").split(/(?=[A-Z])/g);
+  return nonumber.join("-").toLowerCase();
 };
 console.log(kebabize("camelsHaveThreeHumps"));
 console.log(kebabize("camelsHave3Humps"));
