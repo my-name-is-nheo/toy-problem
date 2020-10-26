1984837830e662a8ad8e60cef399a88a2a2808c4
Nicholas Heo <nicholas.heo95@gmail.com>
2020-10-26 17:10:36 -0400

    progress

--- kebabize.js
+++ kebabize.js
@@ -8,4 +8,20 @@ Notes:
 the returned string should only contain lowercase letters
 */
 
-var kebabize = (string) => {};
+var kebabize = (string) => {
+  var wordContainer = [];
+  var wordToPush = "";
+  for (var index = 0; index < string.length; index++) {
+    var currentChar = string[index];
+    if (currentChar === currentChar.toUpperCase()) {
+      wordContainer.push(wordToPush);
+      wordToPush = "";
+      wordToPush += currentChar;
+    } else {
+      wordToPush += currentChar;
+    }
+  }
+  console.log(wordContainer);
+};
+console.log(kebabize("camelsHaveThreeHumps"));
+console.log(kebabize("camelsHave3Humps"));
