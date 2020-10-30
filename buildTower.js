9350b1c7651a2b9450857762924341967cfe2072
Nicholas Heo <nicholas.heo95@gmail.com>
2020-10-30 12:01:15 -0400

    attempt

--- buildTower.js
+++ buildTower.js
@@ -15,4 +15,14 @@ Ruby: returns an Array;
 Lua: returns a Table;
 Have fun!
 */
-var towerBuilder = (nFloors) => {};
+var towerBuilder = (nFloors) => {
+  var tower = [];
+  for (var i = 0; i < nFloors; i++) {
+    tower.push(
+      " ".repeat(nFloors - i - 1) +
+        "*".repeat(i * 2 + 1) +
+        " ".repeat(nFloors - i - 1)
+    );
+  }
+  return tower;
+};
