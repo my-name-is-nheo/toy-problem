056038fbba173cfcd92f1e881b79b26adda685c7
Nicholas Heo <nicholas.heo95@gmail.com>
2020-10-29 21:03:04 -0400

    initial

---
+++ buildTower.js
@@ -0,0 +1,18 @@
+/*
+Build Tower
+Build Tower by the following given argument:
+number of floors (integer and always greater than 0).
+
+Tower block is represented as *
+
+Python: return a list;
+JavaScript: returns an Array;
+C#: returns a string[];
+PHP: returns an array;
+C++: returns a vector<string>;
+Haskell: returns a [String];
+Ruby: returns an Array;
+Lua: returns a Table;
+Have fun!
+*/
+var towerBuilder = (nFloors) => {};
