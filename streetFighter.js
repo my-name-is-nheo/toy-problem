be7e7e91e3d42464c8b1aad115797f8b6160a411
Nicholas Heo <nicholas.heo95@gmail.com>
2020-11-01 11:57:55 -0500

    attempt

--- streetFighter.js
+++ streetFighter.js
@@ -72,5 +72,21 @@ OFF-TOPIC
 Some music to get in the mood for this kata :
 */
 var streetFighterSelection = (fighters, position, moves) => {
-  return "";
+  var result = [];
+
+  moves.forEach(function (move) {
+    if (move === "up") {
+      position[0] = 0;
+    } else if (move === "down") {
+      position[0] = 1;
+    } else if (move === "right") {
+      position[1] = position[1] === 5 ? 0 : position[1] + 1;
+    } else if (move === "left") {
+      position[1] = position[1] === 0 ? 5 : position[1] - 1;
+    }
+
+    result.push(fighters[position[0]][position[1]]);
+  });
+
+  return result;
 };
