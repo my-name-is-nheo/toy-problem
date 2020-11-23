84348bb0fe21fd368d6787e173833cc232d6148e
Nicholas Heo <nicholas.heo95@gmail.com>
2020-11-22 22:12:34 -0500

    initial

--- SImplifyingMultiLinearPolynomials.js
+++ SImplifyingMultiLinearPolynomials.js
@@ -1,19 +1,16 @@
 /*
-When we attended middle school were asked to simplify mathematical expressions like "3x-yx+2xy-x" (or usually bigger), and that was easy-peasy ("2x+xy"). But tell that to your pc and we'll see!
+John has invited some friends. His list is:
 
-Write a function: simplify, that takes a string in input, representing a multilinear non-constant polynomial in integers coefficients (like "3x-zx+2xy-x"), and returns another string as output where the same expression has been simplified in the following way ( -> means application of simplify):
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+Could you make a program that
 
-All possible sums and subtraction of equivalent monomials ("xy==yx") has been done, e.g.:
-"cb+cba" -> "bc+abc", "2xy-yx" -> "xy", "-a+5ab+3a-c-2a" -> "-c+5ab"
+makes this string uppercase
+gives it sorted in alphabetical order by last name.
+When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
 
+So the result of function meeting(s) will be:
 
-All monomials appears in order of increasing number of variables, e.g.:
-"-abc+3a+2ac" -> "3a+2ac-abc", "xyz-xz" -> "-xz+xyz"
-
-If two monomials have the same number of variables, they appears in lexicographic order, e.g.:
-"a+ca-ab" -> "a-ab+ac", "xzy+zby" ->"byz+xyz"
-
-There is no leading + sign if the first coefficient is positive, e.g.:
-"-y+x" -> "x-y", but no restrictions for -: "y-x" ->"-x+y"
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+It can happen that in two distinct families with the same family name two people have the same first name too.
 */
-var simplify = (poly) => {};
+var meeting = (s) => {};
