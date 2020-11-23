428ecf9aaabab8a564c6ab2ebf9819d8db41a2e5
Nicholas Heo <nicholas.heo95@gmail.com>
2020-11-22 21:56:35 -0500

    attempt1

--- isIntArray.js
+++ isIntArray.js
@@ -9,4 +9,26 @@ returns true / True if array is empty.
 returns false / False for every other input.
 */
 
-var isIntArray = (arr) => {};
+var isIntArray = (arr) => {
+  if (Array.isArray(arr)) {
+    if (arr.length === 0) {
+      return true;
+    } else {
+      var result;
+      for (var index = 0; index < arr.length; index++) {
+        var currentElement = arr[index];
+        if (currentElement === null) {
+          return false;
+        }
+
+        if (Number.isInteger(currentElement) || currentElement % 1 === 0) {
+          result = true;
+        } else {
+          return false;
+        }
+      }
+      return result;
+    }
+  }
+  return false;
+};
